0a2bd94615016c674fb0a01eb51f1cbf904784e5
unknown <isedin_k@yahoo.com>
2024-07-26 09:28:13 +0200

    projekt started

---
+++ script.js
@@ -0,0 +1,7 @@
+const calculate = () => {
+    const value = document.querySelector("#numbers").value;
+    // Use the /,\s*/g regex to split the value string by commas. You can tweak it based on the number of spaces separating your values. Store the array in an array variable.
+    // const array = value.split(/,\s*/g);
+    // Create a numbers variable and assign it the value of array.map(). Remember that .map() creates a new array, instead of mutating the original array.
+    const numbers = array.map(Number);
+  }
